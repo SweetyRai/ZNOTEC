425e183c09062495f3486054fc8a1ef8e1797d16
unknown <Sweety Rai>
2025-03-13 12:47:22 +0100

    added Link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Footer = () => {
